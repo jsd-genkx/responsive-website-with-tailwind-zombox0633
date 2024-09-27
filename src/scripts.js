ded9a39bc2c73bc931d642b910a7eec9f5c83a6b
Zombox <ghostzb@outlook.com>
2024-09-27 11:18:43 +0700

    add scripts.js and edit design section menu-burgers

---
+++ src/scripts.js
@@ -0,0 +1,13 @@
+function toggleMobileMenu() {
+  const menu = document.getElementById("menu");
+  menu.classList.toggle("hidden");
+}
+// Close the mobile menu when a link is clicked
+const menuLinks = document.querySelectorAll("#menu a");
+menuLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    if (window.innerWidth < 768) {
+      toggleMobileMenu();
+    }
+  });
+});
